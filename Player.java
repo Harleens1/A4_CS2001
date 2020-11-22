a20a7f7ba95a476eb3b3f93e59b23c6580c336f9
Sheppard2000 <71345815+Sheppard2000@users.noreply.github.com>
2020-11-22 14:18:07 -0330

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,1 +1,27 @@
 
+
+/**
+ * The Player class is supposed to create objects of type Player.
+ * That is to say that the player class creates distincit objects
+ * for each player of each team within the volleyball league.
+ *
+ * @author (Thomas Sheppard)
+ * @version (v1)
+ */
+public class Player
+{
+    // Class instance variables. Name is the name of the player and id
+    // is the number associated with that player.
+    private String name;
+    private int id;
+    
+    /**
+     * Creates a player with the associated name and id number.
+     */
+    public Player(String name, int id)
+    {
+        // initialise instance variables
+        this.name = name;
+        this.id = id;
+    }
+}
