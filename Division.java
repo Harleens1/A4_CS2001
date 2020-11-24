5a9152909d0f36d6ee06b0e59d2c41a5bf86acb3
Sheppard2000 <71345815+Sheppard2000@users.noreply.github.com>
2020-11-24 18:59:55 -0330

    Update Division.java

--- Division.java
+++ Division.java
@@ -22,10 +22,34 @@ public class Division
     }
 
     /**
-     * This method adds a team to the division.
+     * This method adds teams to the division.
      */
     public void addTeam(String teamName)
     {
         teams.add(new Team(teamName));
+    }
+    
+    /**
+     * This method returns a team at a specific index within the divison.
+     */
+    public Team getTeam(int index)
+    {
+        return teams.get(index);
+    }
+    
+    /**
+     * This method adds a team member to a team within the divison.
+     */
+    public void addTeamMember(int index, String n, int id)
+    {
+        teams.get(index).addPlayer(n, id);
+    }
+    
+    /**
+     * This method adds a coach to a team within the division.
+     */
+    public void addTeamCoach(int index, String n, int id)
+    {
+        teams.get(index).addCoach(n, id);
     }
     
