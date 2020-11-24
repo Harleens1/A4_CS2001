497c5141004d3c770ff8f5b381e6b2019ee48a6f
Sheppard2000 <71345815+Sheppard2000@users.noreply.github.com>
2020-11-24 16:11:09 -0330

    Update Division.java

--- Division.java
+++ Division.java
@@ -1,1 +1,31 @@
+import java.util.ArrayList;
 
+/**
+ * The Division class creates divisions. Each division has a name, a list
+ * of teams, and a list of matches between teams of that division.
+ */
+public class Division
+{
+    // instance variables - replace the example below with your own
+    private String name;
+    private ArrayList<Team> teams;
+    private ArrayList<Match> matches;
+
+    /**
+     * Constructor for objects of class Division
+     */
+    public Division(String name)
+    {
+        this.name = name;
+        teams = new ArrayList<>();
+        matches = new ArrayList<>();
+    }
+
+    /**
+     * This method adds teams to the division.
+     */
+    public void addTeam(String teamName)
+    {
+        teams.add(new Team(teamName));
+    }
+    
