865750f584c2a1df419be8d9428eb522a4f4ba4b
Harleens <48388721+Harleens1@users.noreply.github.com>
2020-11-25 09:54:07 -0330

    Update League.java

--- League.java
+++ League.java
@@ -10,7 +10,9 @@ public class League
     // Instance variables include the name of the league and a list of
     // the divisions in the league.
     private String name;
-    private ArrayList<Division> divisions;
+    private ArrayList<Division> divisions; 
+    private ArrayList<Player> players; 
+    private ArrayList<Coach> coaches;
 
     /**
      * Constructor for objects of class League. Creates an ArrayList
@@ -20,7 +22,9 @@ public class League
     {
         // initialise instance variables
         this.name = name;
-        divisions = new ArrayList<>();
+        divisions = new ArrayList<>(); 
+        players =  new ArrayList<>(); 
+        coaches = new ArrayList<>();
         divisions.add(new Division("Division1"));
         divisions.add(new Division("Division2"));
         divisions.add(new Division("Division3"));
@@ -46,7 +50,31 @@ public class League
         divisions.get(2).addTeam("Team2");
         divisions.get(2).addTeam("Team3");
         divisions.get(2).addTeam("Team4");
-        divisions.get(2).addTeam("Team5");
-        
+        divisions.get(2).addTeam("Team5");   
+        int players = 1; 
+        int teams = 1;
+        int div = 1;
+        while(players<=12 && div<=3){
+            divisions.get(div).getTeam(teams).addPlayer("player" + players, players); 
+            players++; 
+            if(players == 12){
+                teams++; 
+                players = 0; 
+                if(teams==5){
+                    div++; 
+                    teams = 0;
+                }
+            }
+        }
+    } 
+    
+    public void addPlayer(String name, int i)
+    {
+        Player p = new Player(name, i);
+    } 
+    
+    public void addCoach(String name, int i) 
+    { 
+        Coach c = new Coach(name, i);
     }
 }
