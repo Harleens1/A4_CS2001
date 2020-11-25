9ca168574fd40b9574bd169442f89fa32d4efb2b
Sheppard2000 <71345815+Sheppard2000@users.noreply.github.com>
2020-11-25 13:00:45 -0330

    Update League.java

--- League.java
+++ League.java
@@ -69,6 +69,61 @@ public class League
         }
     } 
     
+    // Revised setupLeague method.
+    /**
+     * The setupLeague method creates teams and adds them to the divisions
+     * in the league.
+     */
+    public void setupLeague()
+    {
+        divisions.get(0).addTeam("Team1");
+        divisions.get(0).addTeam("Team2");
+        divisions.get(0).addTeam("Team3");
+        divisions.get(0).addTeam("Team4");
+        divisions.get(0).addTeam("Team5");
+        divisions.get(1).addTeam("Team1");
+        divisions.get(1).addTeam("Team2");
+        divisions.get(1).addTeam("Team3");
+        divisions.get(1).addTeam("Team4");
+        divisions.get(1).addTeam("Team5");
+        divisions.get(2).addTeam("Team1");
+        divisions.get(2).addTeam("Team2");
+        divisions.get(2).addTeam("Team3");
+        divisions.get(2).addTeam("Team4");
+        divisions.get(2).addTeam("Team5");   
+        int p = 0; 
+        int t = 0;
+        int d = 0;
+        while(p<=12 && d<3){
+            divisions.get(d).getTeam(t).addPlayer("player" + p, p);
+            p++; 
+            if(p == 12){
+                t++; 
+                p = 0; 
+                if(t==4){
+                    d++; 
+                    t = 0;
+                }
+            }
+        }
+        int c = 0;
+        t = 0;
+        d = 0;
+        while(c<2 && d<3){
+            divisions.get(d).getTeam(t).addCoach("Coach" + c, c);
+            c++;
+            if(c == 2){
+                t++;
+                c = 0;
+                if(t == 4){
+                    d++;
+                    t = 0;
+                }
+            }
+        }
+    } 
+    // revised method ends.
+    
     public void addPlayer(String name, int i)
     {
         Player p = new Player(name, i);
